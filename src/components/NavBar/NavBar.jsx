03b56e3de86769fcefb338143c238deca812b957
Gopal Reddy Devarakonda <gopalreddydevarakonda@gmail.com>
2023-11-26 21:00:45 +0530

    update navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 
 const NavBar = () => {
     return (
-        <div className='grid grid-cols-12 bg-black text-white font-semibold text-[14px] px-3 rounded-lg py-2'>
-            <div className='col-span-2'>
+        <div className='grid grid-cols-12 bg-black text-white font-semibold text-[10px] sm:text-[14px] px-3 rounded-lg py-2'>
+            <div className='col-span-3 sm:col-span-2'>
                 Categories
             </div>
-            <div className='col-span-2'>
+            <div className='col-span-3 sm:col-span-2'>
                 Activites Near Me
             </div>
-            <div className='col-span-2'>
+            <div className='col-span-3 sm:col-span-2'>
                 Add a Place
             </div>
-            <div className='col-span-6 '>
+            <div className='col-span-3 sm:col-span-6 '>
                 <div className='float-right flex gap-2 items-center'>
                     Start exploring
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
