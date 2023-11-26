65080257bc6695a325ead7a4a95adc34731a8346
Gopal Reddy Devarakonda <gopalreddydevarakonda@gmail.com>
2023-11-26 20:50:39 +0530

    update google map location

--- src/components/Location/Map.jsx
+++ src/components/Location/Map.jsx
@@ -15,9 +15,9 @@ const LocationPin = ({ text }) => (
 const Location = () => {
 
   const location = {
-    address: '1600 Amphitheatre Parkway, Mountain View, california.',
-    lat: 37.42216,
-    lng: -122.08427,
+    address: 'Tsukishima Monja Street',
+    lat: 35.6643,
+    lng: 139.7825,
   }
 
   return (
